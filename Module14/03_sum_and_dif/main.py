c04be1a64533921f160f0b3319ab154d8f392635
Роман Ермолаев <9868.08@gmail.com>
2021-05-13 15:05:30 +0400

    os info

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+def numbers_summ (num):
+    out=0
+    while num != 0:
+        out=out+num % 10
+        num = num // 10
+    return int(out)
+def numbers_quantity(num):
+    out=0
+    while num != 0:
+        out+=1
+        num = num // 10
+    return int(out)
+
+inc = int(input('введите число: '))
+#inc=322
+print ('Сумма чисел числа',inc,'равна',numbers_summ(inc))
+print ('Количество чисел числа',inc,'равна',numbers_quantity(inc))
--- Module14/04_reverse_num/README.md
+++ Module14/04_reverse_num/README.md
@@ -10,3 +10,13 @@
 Второе число наоборот: 321.43
 Сумма: 522.64
 ```
+def num_backward (num):
+    count = 1
+    digit=""
+    out=""
+    for digit in str(inc):
+        out=digit+out
+        print('число наоборот: ', digit,out)
+        return(out)
+num1=123
+print (num_backward(num1))
