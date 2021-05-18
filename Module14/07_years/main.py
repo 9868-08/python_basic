9d6b4d6eeff237f0aceef9b929fe9e614ae1ba10
Роман Ермолаев <9868.08@gmail.com>
2021-05-18 10:32:44 +0400

    module_7

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,15 +1,16 @@
-A=1911
-B=2100
+start=1900
+last_year=start-1
+end=2100
 
-for num in range(A,B):
+for year in range(start,end):      #перебираем года
     count = 0
-    for checkin_digit in str(num):
-        for digit in str(num):
-            if checkin_digit==digit:
+    for external_cycle_digit in str(year):    #перебираем цифры в числе - год
+        for internal_cycle_digit in str(year):    #и сравниваем с каждой другой цифрой года
+            if internal_cycle_digit==external_cycle_digit:
                 count+=1
-            if count == 3:
-                print(num)
+            if count == 3 and last_year!=year:
+                print(year)
                 count = 0
+                last_year=year
         count = 0
-        break
-#print('итого:',result)
+
