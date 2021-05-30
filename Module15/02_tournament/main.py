8b74508be569e8838c7412e36901ed241b540d34
Роман Ермолаев <9868.08@gmail.com>
2021-05-30 13:41:03 +0400

    module15 rm --cached

--- Module15/02_tournament/main.py
+++
@@ -1,6 +0,0 @@
-list = ['Артемий', 'Борис', 'Влад', 'Гоша', 'Дима', 'Евгений', 'Женя', 'Захар']
-count=0
-for value in list:
-    if count%2==1:
-        print(list[count])
-    count+=1
--- Module15/09_word_analysis_2/main.py
+++
@@ -1,9 +0,0 @@
-inc = 'мадам'
-count = 0
-flag = 0
-for letter in inc:
-    if not letter == inc [len(inc) - count - 1]:
-        flag=1
-    count += 1
-if flag == 0:
-    print('слово "',inc,'" является полиндромом')
