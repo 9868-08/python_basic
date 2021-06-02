9743b2ddaf115d52dc021ff4b05d322afb58ee09
Роман Ермолаев <9868.08@gmail.com>
2021-06-02 22:53:31 +0400

    06_word_analysis\main.py according teacher help

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,22 +1,10 @@
-# Define the custom function to find element in the list
-def findElement(listName, searchElement):
-  # Read the list using loop
-  for value in listName:
-  # Check the element value is equal to the search value or not
-    if value == searchElement:
-      return True
-
-    # Return false if no match found
-    return False
-
 input_word = 'лава'
-output=[]
+output = []
 letter_count = 0
 non_uniq_count = 0
 count_inside = 0
 count_outside = 0
-symbols = []
 for symbol_outside in input_word:
     if symbol_outside not in input_word:  # если символа нет в списке
-        symbols.append(sym)
-print(symbols)
+        output.append(symbol_outside)
+print('уникальные буквы слове', input_word, ' - ', output)
