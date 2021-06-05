08f2c2eeeba777e4bb3905ad12d227103065130d
Роман Ермолаев <9868.08@gmail.com>
2021-06-05 16:47:03 +0400

    teacher helped

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -5,6 +5,6 @@ non_uniq_count = 0
 count_inside = 0
 count_outside = 0
 for symbol_outside in input_word:
-    if symbol_outside not in input_word:  # если символа нет в списке
+    if symbol_outside not in output:  # если символа нет в списке
         output.append(symbol_outside)
-print('уникальные буквы слове', input_word, ' - ', output)
+print('уникальных букв в слове', input_word, ' - ', len(output))
