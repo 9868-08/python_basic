3bac931660a7a0236841ea34eff4a362f4f13d03
Роман Ермолаев <9868.08@gmail.com>
2021-06-15 19:23:13 +0400

    08 input added

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,6 +1,6 @@
 from random import randrange
-
-N = 10  # количество человек в кругу
+#N = 10  # количество человек в кругу
+N=int(input('Какое количество человек будет играть? '))
 list = list(set(range(N)))
 print('исходный список:', list)
 while (not len(list) == 1):
