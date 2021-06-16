41f25da7d288a96ca380a99a43334fb29cf26c6f
Роман Ермолаев <9868.08@gmail.com>
2021-06-16 17:11:41 +0400

    module8

--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,19 +1,22 @@
 from random import randrange
 
 N = 3
-print('Количество друзей:',N)
+print('Количество друзей:', N)
 K = 10
-print('# количество расписок:',K)
+print('# количество расписок:', K)
 
 friends = set(range(N))
 credits = []  # ["кто", "кому" , "сколько"]
 list(credits)
-for num in set(range(0,K)):
+for num in set(range(0, K)):
     credits.append([randrange(N), randrange(N), randrange(10)])
 print(credits)
-personal = []
-for num in set(range(0,K)):
-#    credits.append([randrange(N), randrange(N), randrange(10)])
-    print (credits[num][1],credits[num][2])
-    personal(credits[num][1])+=credits[num][2]
-print(personal)
+
+personal = [set(range(0, N))]
+for IOU in set(range(0, K)):  # IOU - долговая расписка
+    print(credits[IOU], credits[IOU][0],credits[IOU][1], credits[IOU][2])
+#    personal[credits[IOU][1]] = credits[IOU][2]
+#    personal[credits[IOU][1]] -= credits[IOU][0]
+
+
+print (personal)
