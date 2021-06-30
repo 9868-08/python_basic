45af484d0c18317547b811c8748d863f680e275e
Роман Ермолаев <9868.08@gmail.com>
2021-06-30 10:30:20 +0400

    1 with dublicates

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,12 +1,19 @@
-vowels_list = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е']
-#input_text='Нужно отнести кольцо в Мордор!'
-input_text=input('Введите текст для проверки: ')
-input_vovwels=[]
-
-for input_character in input_text:
+def check_vowel (sybmol,input_vovwels,flag):
+    vowels_list = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е']
+    inp_char_count=0
     for vowel in vowels_list:
-        if input_character == vowel:
+        if sybmol == vowel and not flag == 1:
             input_vovwels.append(vowel)
+    flag = 1
+    return input_vovwels,flag
+
+
+input_text='Нужно отнести кольцо в Мордор!'
+#input_text=input('Введите текст для проверки: ')
+input_vovwels=[]
+flag=0
+input_vovwels=[check_vowel(sybmol,input_vovwels,flag) for sybmol in input_text]
+
 
 print('Список гласных букв: ', input_vovwels)
 print('Длина списка: ', len(input_vovwels))
