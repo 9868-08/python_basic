640a8ae7b7d765a112bbfb5e7cc26a26d90536cd
Роман Ермолаев <9868.08@gmail.com>
2021-06-30 21:36:02 +0400

    1 from teacher help

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,19 +1,7 @@
-def check_vowel (sybmol,input_vovwels,flag):
-    vowels_list = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е']
-    inp_char_count=0
-    for vowel in vowels_list:
-        if sybmol == vowel and not flag == 1:
-            input_vovwels.append(vowel)
-    flag = 1
-    return input_vovwels,flag
-
-
 input_text='Нужно отнести кольцо в Мордор!'
 #input_text=input('Введите текст для проверки: ')
-input_vovwels=[]
-flag=0
-input_vovwels=[check_vowel(sybmol,input_vovwels,flag) for sybmol in input_text]
 
+vowels = [i for i in input_text if i in 'ауоыиэяюёе']
 
-print('Список гласных букв: ', input_vovwels)
-print('Длина списка: ', len(input_vovwels))
+print('Список гласных букв: ', vowels)
+print('Длина списка: ', len(vowels))
