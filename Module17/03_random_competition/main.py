f8f430eeefc8e4f3ae0bc262984046d4511e3d2f
Roman <9868.08@gmail.com>
2021-07-08 17:52:29 +0400

    module 3 (teacher helped)

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -7,7 +7,8 @@ print ('Очки участников первой команды: ',team1)
 team2 = [round(random.uniform(0,10),2) for i in range(20)]
 print ('Очки участников второй команды: ',team2)
 
-team3 = [team1[i] for i in range(20) if team1[i]>team2[i]]
-team3 = [team2[i] for i in range(20) if team1[i]>team2[i]]
-
+team3 = [team1[i_player]
+           if team1[i_player] > team2[i_player]
+           else team2[i_player]
+           for i_player in range(20)]
 print ('Очки участников побед-лей тура: ',team3)
