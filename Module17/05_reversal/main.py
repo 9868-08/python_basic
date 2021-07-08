9f261217fe02676bdfa925f000e2e18ee686fb09
Roman <9868.08@gmail.com>
2021-07-08 19:51:36 +0400

    module 5 (without conprehension)

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -1,9 +1,17 @@
-input='Как упоительны в hРоссииh вечера'
-#vowels = [i for i in input_text if i in 'ауоыиэяюёе']
+input_str='Как упоительны в hРоссииh вечера'
+#input_str=input('Введите строку: ')
 
-h_count=0
-ouput = [input[i]
-           if input[i] == 'h' and h_count == 0
-           elif input[i] == 'h' and h_count == 1
-           for i in range(20)]
-print ('Очки участников побед-лей тура: ',team3)
+result = ""
+flag = 0
+
+#vowels = [i for i in input_text if i in 'ауоыиэяюёе']
+for i in input_str:
+    if i == 'h' and flag == 1:
+        flag=0
+    elif flag==1:
+        result=result+i
+    elif i == 'h' and flag == 0:
+        flag=1
+
+
+print ('Между "h" заключена последовательность символов:',result)
