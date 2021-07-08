91b6c237101748bdde1ca134d1716037a47dd6e8
Roman <9868.08@gmail.com>
2021-07-08 18:18:59 +0400

    module 5 unwork

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+input='Как упоительны в hРоссииh вечера'
+#vowels = [i for i in input_text if i in 'ауоыиэяюёе']
+
+h_count=0
+ouput = [input[i]
+           if input[i] == 'h' and h_count == 0
+           elif input[i] == 'h' and h_count == 1
+           for i in range(20)]
+print ('Очки участников побед-лей тура: ',team3)
