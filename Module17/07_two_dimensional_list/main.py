9ba8979b05447e22424cafcfe342dac0eb2ecddd
Roman <9868.08@gmail.com>
2021-07-09 11:55:44 +0400

    module 7 unwork

--- Module17/07_two_dimensional_list/main.py
+++ Module17/07_two_dimensional_list/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+from random import randint
+
+list = []
+N = 5
+
+#list = [randint(0,9),randint(10,19) for i in range(N)]
+for range(N):
+    list.append(random(N),random(N))
+
+print('изначальный список:', list)
