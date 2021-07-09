25f4734384e3972da90c1ce2ac2d867b73e4118e
Roman <9868.08@gmail.com>
2021-07-09 11:20:45 +0400

    module 5

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -1,25 +1,5 @@
-input_str='Как упоительны в hРоссииh вечера'
-#input_str=input('Введите строку: ')
+#input_str='Как hупоительны вh России вечера'
+input_str=input('Введите строку: ')
 
-# result = ""
-# flag = 0
-# for i in input_str:
-#     if i == 'h' and flag == 1:
-#         flag=0
-#     elif flag==1:
-#         result=result+i
-#     elif i == 'h' and flag == 0:
-#         flag=1
-#
-# team3 = [team1[i]
-#            if team1[i] > team2[i]
-#            else team2[i]
-#            for i in range(20)]
-# print ('Очки участников побед-лей тура: ',team3)
-
-result = [i
-            if i == 'h' and flag == 1
-            else flag == 0
-          for i in input_str]
-
-print ('Между "h" заключена последовательность символов:\n',result)
+result = [i for i in range(len(input_str)) if input_str[i] == 'h'  ]
+print ('Между "h" заключена последовательность символов:\n',input_str[result[0]+1:result[1]])
