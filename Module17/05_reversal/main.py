d7af02694cccb1866442aa3956e7489388d6dd6e
Roman <9868.08@gmail.com>
2021-07-10 20:34:00 +0400

    module 5. add reverse

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -1,5 +1,6 @@
-#input_str='Как hупоительны вh России вечера'
-input_str=input('Введите строку: ')
+input_str='Как упоительны в hРоссииh вечера'
+#input_str=input('Введите строку: ')
 
-result = [i for i in range(len(input_str)) if input_str[i] == 'h'  ]
-print ('Между "h" заключена последовательность символов:\n',input_str[result[0]+1:result[1]])
+result = [i for i in range(len(input_str)) if input_str[i] == 'h']
+ouput_str = input_str[result[0]+1:result[1]]
+print ('Между "h" заключена последовательность символов:\n',ouput_str[::-1])
