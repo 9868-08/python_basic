c463997e19f34572c88a091aca4b5a21397b00cf
Roman <9868.08@gmail.com>
2021-07-12 15:35:33 +0400

    module 9, module 10

--- Module17/10_caesar_cipher/main.py
+++ Module17/10_caesar_cipher/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+#message='это питон'
+message=input('Введите сообщение прописнными буквами: ')
+#k = 3
+k=int(input('Введите сдвиг: '))
+
+abc='абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+count=1
+ceaser_cipher=''
+for letter in message:
+    if letter==" ":
+        new_index=index
+        ceaser_cipher+=' '
+    else:
+        index = abc.index(letter)
+        new_index=index+3
+        ceaser_cipher+=abc[new_index]
+print('Зашифрованное сообщение: ', ceaser_cipher)
