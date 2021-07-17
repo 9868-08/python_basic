bd6728a411dcd164336c977800a00ea6b443b317
Роман Ермолаев <9868.08@gmail.com>
2021-07-17 11:44:10 +0400

    cesar_ciphr unwork

--- Module17/10_caesar_cipher/main.py
+++ Module17/10_caesar_cipher/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def ceaser_cipher (string, user_shift):
+    char_list = [(abc[(abc.index(sym) + user_shift) % 33] for sym != ' ' else ' ') in string]
+    new_str = ''
+    for i_char in char_list:
+        new_str += i_char
+    return new_str
+
+message = 'это питон'
+#message=input('Введите сообщение прописнными буквами: ')
+k = 3
+#k=int(input('Введите сдвиг: '))
+abc='абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+output_str = ceaser_cipher(message, k)
+print('Зашифрованное сообщение: ', output_str)
