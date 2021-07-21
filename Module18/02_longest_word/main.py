f386b4be75700be3859a4720c625addb1a55be50
Роман Ермолаев <9868.08@gmail.com>
2021-07-21 20:43:04 +0400

    02

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,4 +1,11 @@
-text = 'мой дядя самых честных правил'
+text = 'абрашварбакадабра. мой дядя самых честных правил'
 #text = input('Введите содержимое файла: ')
 words_list = text.split()
 print (words_list)
+
+max_len_word = ''
+for i in words_list:
+    if len(i)>len(max_len_word):
+        max_len_word=i
+        print('длина слова', i, 'равна', len(i))
+print('Самое длииное слово: ', max_len_word,'его длина ',len(max_len_word))
