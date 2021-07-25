c20928b2e0281fac07646f324b0f85a0cdeda876
Роман Ермолаев <9868.08@gmail.com>
2021-07-25 16:06:30 +0400

    03

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+#filename='@example.txt'
+filename=input('Название файла: ')
+
+for i in filename:
+    if i == '@' or i == '№' or i == '$' or i == '%' or i == '^' or i == '&' or i == '*' or i == '(' or i == ')':
+        print ('файл: ', filename, 'имеет недопустимый символ в имени')
+if filename[(len(filename)-4):] == '.txt' or filename[(len(filename)-4):] == '.docx':
+    print ('',end='')
+else:
+    print('файл: ', filename, 'имеет неправильное расширение', filename[(len(filename) - 4):])
