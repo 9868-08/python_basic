3f1713754b161e65deff7e71591d5d2bbd29f08e
Роман Ермолаев <9868.08@gmail.com>
2021-08-02 09:51:14 +0400

    5

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -4,21 +4,23 @@ def password_check (text):
     NUM = '1234567890'
     uppercase_flag = 0
     num_flag = 0
+    count = 0
     for i in text:
-        if not ABC.rfind(i) == -1:
-            print(i,'- заглавная буква','ABC.rfind(i)',ABC.rfind(i))
+        if i in ABC:
+#            print(i,'- заглавная буква','ABC.rfind(i)',ABC.rfind(i))
             uppercase_flag +=1
-        if not NUM.rfind(i) == -1:
-            print(i, '- цифра', 'NUM.rfind(i)', NUM.rfind(i))
+        elif i in NUM:
+#            print(i, '- цифра', 'NUM.rfind(i)', NUM.rfind(i))
             num_flag += 1
-    print(uppercase_flag)
-    if uppercase_flag > 2 and num_flag != 0 :
-        return 0
+        count+=1
+    print('В введённом пароле', text,'букв верхнего регистра:', uppercase_flag, 'Цифр: ', num_flag)
+    if uppercase_flag > 1 and num_flag > 3 and count > 8 :
+        return 1    # 1 - требования выполнены
     else:
-        return 1
+        return 0
 
 
 password = ' '
-while password_check(password) != 0:
+while password_check(password) == 0:
     password = input ('Придумайте пароль: ')
     print('пароль не удовлетворяет условиям')
