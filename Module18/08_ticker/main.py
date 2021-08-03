b98bd646b8dfcbf2a54f9da308e6bb27c92e2b1a
Роман Ермолаев <9868.08@gmail.com>
2021-08-03 17:40:57 +0400

    7 with no brakes

--- Module18/08_ticker/main.py
+++ Module18/08_ticker/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+import re
+
+inc = 'Это задание очень! простое'
+list = re.split('' ' |, |\*|!', inc)
+out = ''
+for word in list:
+    reverse_word = ''
+    count = 1
+    for i in word:
+        reverse_word = reverse_word + word[len(word) - count]
+        count += 1
+    out += reverse_word
+    print(out)
