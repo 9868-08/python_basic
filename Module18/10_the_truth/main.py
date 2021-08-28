cef4c2692b0640d18fb95569ac2ce3aa491bd274
Роман Ермолаев <9868.08@gmail.com>
2021-08-28 13:19:36 +0400

    10 unworks

--- Module18/10_the_truth/main.py
+++ Module18/10_the_truth/main.py
@@ -14,7 +14,7 @@ def caesar_cipher(message, shift):
     count = 1
     ceaser_cipher = ''
     for letter in message:
-        print('letter=', letter)   #debug output
+#        print('letter=', letter)   #debug output
         if letter == " ":
             new_index = index
             ceaser_cipher += ' '
@@ -24,6 +24,11 @@ def caesar_cipher(message, shift):
             ceaser_cipher += '('
         elif letter == "-":
             ceaser_cipher += '-'
+        elif letter == "+":
+            ceaser_cipher += '+'
+        elif letter == '"':
+            ceaser_cipher += '"'
+
         elif letter == " ":
             ceaser_cipher += ' '
         elif letter == ".":
@@ -36,4 +41,4 @@ def caesar_cipher(message, shift):
 
 
 text = 'vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf ibou fy/dpnqm yDpnqmf jt cfuufs boui dbufe/dpnqmj uGmb tj fuufsc ouib oftufe/ bstfTq jt uufscf uibo otf/ef uzSfbebcjmj vout/dp djbmTqf dbtft (ubsfo djbmtqf hifopv up csfbl ifu t/svmf ipvhiBmu zqsbdujdbmju fbutc uz/qvsj Fsspst tipvme wfsof qbtt foumz/tjm omfttV mjdjumzfyq odfe/tjmf Jo fui dfgb pg hvjuz-bncj gvtfsf fui ubujpoufnq up ftt/hv Uifsf vmetip fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/ Bmuipvhi uibu bzx bzn puo cf wjpvtpc bu jstug ttvomf sfzpv( i/Evud xOp tj scfuuf ibou /ofwfs uipvhiBm fsofw jt fopgu cfuufs boui iu++sjh x/op gJ ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb Jg fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b hppe jefb/ bnftqbdftO bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf"uip'
-print('caesar_cipher=',caesar_cipher(text, 3))
+print('caesar_cipher=',caesar_cipher(text, -3))
