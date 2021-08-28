f64c4f5c7ed0874664a4d581549c7751cba7e2aa
Роман Ермолаев <9868.08@gmail.com>
2021-08-28 11:25:41 +0400

    10 unworks

--- Module18/10_the_truth/main.py
+++ Module18/10_the_truth/main.py
@@ -1,32 +1,33 @@
-def shifter(str,shift):
+def shifter(str, shift):
     count = 0
     result = ''
-    tmp=str+str
+    tmp = str + str
     for i in str:
-        result+=tmp[count+shift]
-        count+=1
+        result += tmp[count + shift]
+        count += 1
     return result
 
-def caesar_cipher(message,shift):
-    abc = 'abcdefghijklmnopqrstuvwxyzABCDEFGJIGKLMNOPQRSTUVY'
+
+def caesar_cipher(message, shift):
+    abc = 'abcdefghijklmnopqrstuvwxyzABCDEFGJIGKLMNOPQRSTUVY()-+."'
     abc += abc
     count = 1
     ceaser_cipher = ''
     for letter in message:
+#        print('letter=', letter)   #debug output
         if letter == " ":
             new_index = index
             ceaser_cipher += ' '
+        elif letter == "/":
+            ceaser_cipher += '\n'
         else:
             index = abc.index(letter)
             new_index = index + shift
             ceaser_cipher += abc[new_index]
-    print('Зашифрованное сообщение: ', ceaser_cipher)
+    print(caesar_cipher)
+    return ceaser_cipher
 
 
-text='vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf ibou fy/dpnqm yDpnqmf jt cfuufs boui dbufe/dpnqmj uGmb tj fuufsc ouib oftufe/ bstfTq jt uufscf uibo otf/ef uzSfbebcjmj vout/dp djbmTqf dbtft (ubsfo djbmtqf hifopv up csfbl ifu t/svmf ipvhiBmu zqsbdujdbmju fbutc uz/qvsj Fsspst tipvme wfsof qbtt foumz/tjm omfttV mjdjumzfyq odfe/tjmf Jo fui dfgb pg hvjuz-bncj gvtfsf fui ubujpoufnq up ftt/hv Uifsf vmetip fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/ Bmuipvhi uibu bzx bzn puo cf wjpvtpc bu jstug ttvomf sfzpv( i/Evud xOp tj scfuuf ibou /ofwfs uipvhiBm fsofw jt fopgu cfuufs boui iu++sjh x/op gJ ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb Jg fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b hppe jefb/ bnftqbdftO bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf"uip'
-
-words_list = text.split('/')
-#print (words_list)
-
-for i in words_list:
-    print(caesar_cipher(i,1))
+#text = 'vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf ibou fy/dpnqm yDpnqmf jt cfuufs boui dbufe/dpnqmj uGmb tj fuufsc ouib oftufe/ bstfTq jt uufscf uibo otf/ef uzSfbebcjmj vout/dp djbmTqf dbtft (ubsfo djbmtqf hifopv up csfbl ifu t/svmf ipvhiBmu zqsbdujdbmju fbutc uz/qvsj Fsspst tipvme wfsof qbtt foumz/tjm omfttV mjdjumzfyq odfe/tjmf Jo fui dfgb pg hvjuz-bncj gvtfsf fui ubujpoufnq up ftt/hv Uifsf vmetip fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/ Bmuipvhi uibu bzx bzn puo cf wjpvtpc bu jstug ttvomf sfzpv( i/Evud xOp tj scfuuf ibou /ofwfs uipvhiBm fsofw jt fopgu cfuufs boui iu++sjh x/op gJ ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb Jg fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b hppe jefb/ bnftqbdftO bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf"uip'
+text = 'vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf ibou fy/dpnqm yDpnqmf jt cfuufs boui dbufe/dpnqmj uGmb tj fuufsc ouib oftufe/ bstfTq jt uufscf uibo otf/ef uzSfbebcjmj vout/dp djbmTqf dbtft (ubsfo djbmtqf hifopv up csfbl ifu t/svmf ipvhiBmu zqsbdujdbmju fbutc uz/qvsj Fsspst tipvme wfsof qbtt foumz/tjm omfttV mjdjumzfyq odfe/tjmf Jo fui dfgb pg hvjuz-bncj gvtfsf fui ubujpoufnq up ftt/hv Uifsf vmetip fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/ Bmuipvhi uibu bzx bzn puo cf wjpvtpc bu jstug ttvomf sfzpv( i/Evud xOp tj scfuuf ibou /ofwfs uipvhiBm fsofw jt fopgu cfuufs boui iu++sjh x/op gJ ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb Jg fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b hppe jefb/ bnftqbdftO bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf"uip'
+print('caesar_cipher=',caesar_cipher(text, -3))
--- Module18/10_the_truth/main_modified.py
+++ Module18/10_the_truth/main_modified.py
@@ -1,3 +1,13 @@
+def decode_text(txt: str):
+    word_decode = ""
+    word_shift = -3   # устанавливаем шифт на -3 изначально
+    for word in txt.split():   # идём циклом по тексту.split(), т.е. делим текст на слова
+        if word.find('/') != -1:   # функция find возвращает -1 если символ, переданный в неё параметром не найден, т.е. если в слове нет /, то:
+            print(word_decode(word, word_shift))   #используя данные функции выводим уже расшифрованное слово
+            word_shift -= 1
+        else:
+            print(word_decode(word, word_shift), end=' ') # обратите внимание на end
+
 def shifter(str,shift):
     count = 0
     result = ''
@@ -26,7 +36,4 @@ def caesar_cipher(message,shift):
 text='vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf ibou fy/dpnqm yDpnqmf jt cfuufs boui dbufe/dpnqmj uGmb tj fuufsc ouib oftufe/ bstfTq jt uufscf uibo otf/ef uzSfbebcjmj vout/dp djbmTqf dbtft (ubsfo djbmtqf hifopv up csfbl ifu t/svmf ipvhiBmu zqsbdujdbmju fbutc uz/qvsj Fsspst tipvme wfsof qbtt foumz/tjm omfttV mjdjumzfyq odfe/tjmf Jo fui dfgb pg hvjuz-bncj gvtfsf fui ubujpoufnq up ftt/hv Uifsf vmetip fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/ Bmuipvhi uibu bzx bzn puo cf wjpvtpc bu jstug ttvomf sfzpv( i/Evud xOp tj scfuuf ibou /ofwfs uipvhiBm fsofw jt fopgu cfuufs boui iu++sjh x/op gJ ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb Jg fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b hppe jefb/ bnftqbdftO bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf"uip'
 #прохожу по каждому слову в списке. Если нахожу в слове '/', то ключ смещения увеличиваю на 1.
 
-str_list = text.split('/')
-
-for i in range(0,50):
-    print(caesar_cipher(str_list[0],i))
+print(decode_text(text))
