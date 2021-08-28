3e63b96af53a8a8885fa93eb30bb7e19cde8e12f
Роман Ермолаев <9868.08@gmail.com>
2021-08-28 13:43:26 +0400

    10_the_truth

--- Module18/10_the_truth/main.py
+++ Module18/10_the_truth/main.py
@@ -1,44 +1,26 @@
-def shifter(str, shift):
-    count = 0
-    result = ''
-    tmp = str + str
-    for i in str:
-        result += tmp[count + shift]
-        count += 1
-    return result
+abc = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+message = 'vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf ibou fy/dpnqm yDpnqmf jt cfuufs boui dbufe/dpnqmj uGmb tj fuufsc ouib oftufe/ bstfTq jt uufscf uibo otf/ef uzSfbebcjmj vout/dp djbmTqf dbtft (ubsfo djbmtqf hifopv up csfbl ifu t/svmf ipvhiBmu zqsbdujdbmju fbutc uz/qvsj Fsspst tipvme wfsof qbtt foumz/tjm omfttV mjdjumzfyq odfe/tjmf Jo fui dfgb pg hvjuz-bncj gvtfsf fui ubujpoufnq up ftt/hv Uifsf vmetip fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/ Bmuipvhi uibu bzx bzn puo cf wjpvtpc bu jstug ttvomf sfzpv( i/Evud xOp tj scfuuf ibou /ofwfs uipvhiBm fsofw jt fopgu cfuufs boui iu++sjh x/op gJ ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb Jg fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b hppe jefb/ bnftqbdftO bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf"uip'
 
 
-def caesar_cipher(message, shift):
-    abc = 'abcdefghijklmnopqrstuvwxyzABCDEFGJIGKLMNOPQRSTUVY'
-    abc += abc
-    count = 1
-    ceaser_cipher = ''
-    for letter in message:
-#        print('letter=', letter)   #debug output
-        if letter == " ":
-            new_index = index
-            ceaser_cipher += ' '
-        elif letter == "/":
-            ceaser_cipher += '\n'
-        elif letter == "(":
-            ceaser_cipher += '('
-        elif letter == "-":
-            ceaser_cipher += '-'
-        elif letter == "+":
-            ceaser_cipher += '+'
-        elif letter == '"':
-            ceaser_cipher += '"'
+tmp = ''
 
-        elif letter == " ":
-            ceaser_cipher += ' '
-        elif letter == ".":
-            ceaser_cipher += '.'
-        else:
-            index = abc.index(letter)
-            new_index = index + shift
-            ceaser_cipher += abc[new_index]
-    return (ceaser_cipher)
+for i_letter in message:
+    if i_letter in abc:
+        text = abc[(abc.index(i_letter) - 1) % 52]
+        tmp += text
+    else:
+        tmp += i_letter
 
+text_decrypted = tmp.split()
+shift = 3
 
-text = 'vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf ibou fy/dpnqm yDpnqmf jt cfuufs boui dbufe/dpnqmj uGmb tj fuufsc ouib oftufe/ bstfTq jt uufscf uibo otf/ef uzSfbebcjmj vout/dp djbmTqf dbtft (ubsfo djbmtqf hifopv up csfbl ifu t/svmf ipvhiBmu zqsbdujdbmju fbutc uz/qvsj Fsspst tipvme wfsof qbtt foumz/tjm omfttV mjdjumzfyq odfe/tjmf Jo fui dfgb pg hvjuz-bncj gvtfsf fui ubujpoufnq up ftt/hv Uifsf vmetip fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/ Bmuipvhi uibu bzx bzn puo cf wjpvtpc bu jstug ttvomf sfzpv( i/Evud xOp tj scfuuf ibou /ofwfs uipvhiBm fsofw jt fopgu cfuufs boui iu++sjh x/op gJ ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb Jg fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b hppe jefb/ bnftqbdftO bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf"uip'
-print('caesar_cipher=',caesar_cipher(text, -3))
+for i_word in range(len(text_decrypted)):
+    for _ in range(shift):
+        text_decrypted[i_word] = text_decrypted[i_word][-1:] + text_decrypted[i_word][:-1]
+    index_symbol = text_decrypted[i_word].find('/')
+    if index_symbol >= 0:
+        text_decrypted[i_word] = text_decrypted[i_word][:index_symbol] + '.'
+        shift += 1
+
+print(' '.join(text_decrypted))
