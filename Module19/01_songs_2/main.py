03bfad7eed7edd218f0f3d8b7726735b2c6a196d
Fomin Nikita <fominquant@yandex.ru>
2021-09-19 20:02:35 +0300

    Need to fix task 4 module 19 homework

--- Module19/01_songs_2/main.py
+++ Module19/01_songs_2/main.py
@@ -1,24 +1,26 @@
 violator_songs = {
-	'World in My Eyes': 4.86,
-	'Sweetest Perfection': 4.43,
-	'Personal Jesus': 4.56,
-	'Halo': 4.9,
-	'Waiting for the Night': 6.07,
-	'Enjoy the Silence': 4.20,
-	'Policy of Truth': 4.76,
-	'Blue Dress': 4.29,
-	'Clean': 5.83
+    'World in My Eyes': 4.86,
+    'Sweetest Perfection': 4.43,
+    'Personal Jesus': 4.56,
+    'Halo': 4.9,
+    'Waiting for the Night': 6.07,
+    'Enjoy the Silence': 4.20,
+    'Policy of Truth': 4.76,
+    'Blue Dress': 4.29,
+    'Clean': 5.83
 }
 # Напишите программу, которая запрашивает у пользователя кол-во песен из списка и затем названия этих песен, а на экран  выводит # общее время их звучания.
-#print (violator_songs)
+# print (violator_songs)
 # count=int(input('Сколько песен выбрать? '))
 count = 3
 selected_songs = []
-#selected_songs = ['Policy of Truth','Enjoy the Silence','Personal Jesus']
-for i in range(0,count):
-	selected_songs.append(input("Введте название песни: "))
+# selected_songs = ['Policy of Truth','Enjoy the Silence','Personal Jesus']
+for i in range(0, count):
+    selected_songs.append(input("Введте название песни: "))
 playing_time = 0
 for i in violator_songs:
-	if i in selected_songs:
-		playing_time += violator_songs[i]
+    if i in selected_songs:
+        playing_time += violator_songs[i]
 print('Общее время звучанния', selected_songs, '=', playing_time)
+
+# зачтено
--- Module19/02_geography/main.py
+++ Module19/02_geography/main.py
@@ -1,12 +1,12 @@
 motherland = dict()
 country_count = int(input('Кол-во стран: '))
-print('Германия Берлин Лейпциг Мюнхен')		# подсказдки,
-print('Россия Москва Петербург Новгород')	# чтобы не набирать
+print('Германия Берлин Лейпциг Мюнхен')  # подсказдки,
+print('Россия Москва Петербург Новгород')  # чтобы не набирать
 for i_country in range(country_count):
     cities_list = input('{} страна: '.format(i_country + 1)).split()
     for j_city in cities_list[1:]:
         motherland[j_city] = cities_list[0]
-print('motherland=',motherland)
+print('motherland=', motherland)
 
 for i_city in range(1, 4):
     city_name = input('{} город: '.format(i_city))
@@ -15,3 +15,5 @@ for i_city in range(1, 4):
             city_name, motherland[city_name]))
     else:
         print('По городу {} данных нет.'.format(city_name))
+
+# зачтено
