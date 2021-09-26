2880aeef894d8b780a35474f0bef235c15582936
Роман Ермолаев <9868.08@gmail.com>
2021-09-26 17:03:42 +0400

    04_goods release

--- Module19/04_goods/main.py
+++ Module19/04_goods/main.py
@@ -37,8 +37,6 @@ for merch, code in goods.items():	# merch-'Лампа', code-'12345',
 	total_quantity = 0
 	total_cost = 0
 	for goods in store[code]:
-		for i in goods:
-			total_quantity += goods['quantity']
-			total_cost += goods['price']*goods['quantity']
-#			print(goods['quantity'],goods['price'])
+		total_quantity += goods['quantity']
+		total_cost += goods['price']*goods['quantity']
 	print(merch, ' - ', total_quantity, 'шт, стоимость', total_cost, 'руб')
