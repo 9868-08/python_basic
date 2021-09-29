bfc82c0b2a38372a79f2949ec7d2301a18c8f50f
Роман Ермолаев <9868.08@gmail.com>
2021-09-29 23:37:31 +0400

    07_pizza in unwork

--- Module19/07_pizza/main.py
+++ Module19/07_pizza/main.py
@@ -9,13 +9,14 @@ for i in range (0, pairs_count):
 #	print('input_dict[input_list[0]]', input_dict[input_list[0]])
 	if input_list[0] not in input_dict:		# если покупатель НЕ находится в словаре заказов:
 		#в словарь по ключу, который является имя покупателя, заносим словарь, ключ которого имя пиццы, а значение - её количество
-		input_dict[input_list[0]] = {input_list[1]:input_list[2]}
+		input_dict[input_list[0]] = {input_list[1]:int(input_list[2])}
+	else:
+		input_dict[input_list[0]] = [input_list[1], input_list[2]]
 		if input_list[1] in input_dict[input_list[0]]:
 			input_dict[input_list[0]][input_list[1]] += int(input_list[2])
 		else:
 			input_dict[input_list[0]].update({input_list[1]: int(input_list[2])})
-	else:
-			input_dict[input_list[0]] = [input_list[1], input_list[2]]
+
 	print(input_dict)
 
 
