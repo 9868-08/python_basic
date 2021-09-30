2c1fb6bb963b8474825dc5a29f10b81d0e1959eb
Роман Ермолаев <9868.08@gmail.com>
2021-09-30 22:37:04 +0400

    08_guess_number release

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+from random import randrange
+
+N = int(input("Введите максимальное число: "))
+#N = 10
+print("Введите максимальное число: ", N)
+x = randrange(N)
+print('загадано число',x)
+answer = 0
+while answer == 0:
+	input_string = input("Нужное число есть среди этих чисел: ")
+	if input_string == 'Помогите!':
+		print("Артем загадал число: ", N)
+		break
+	input_list = input_string.split(' ')
+	for i in input_list:
+		print(i)
+		if int(i) == x:
+			print("Ответ Артёма: Да")
+			answer = 1
+			break
+		else:
+			print("Ответ Артёма: Нет")
