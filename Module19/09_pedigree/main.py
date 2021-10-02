7c43a52fe6a53fbc2b20bad8c934aab09f914eb2
Роман Ермолаев <9868.08@gmail.com>
2021-10-02 16:31:27 +0400

    09_pedigree unwork

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+N = 4	# максимальная высота
+print('Введите количество человек: 9')
+#N = int(input('Введите количество человек: '))
+geno_set=[]
+geno_dict = dict
+for i in range(1, N, 1):
+	print(i, 'пара', end=': ')
+	inc = input()
+	geno_set = inc.split()
+	geno_dict[i] = geno_set
+print(geno_dict)
