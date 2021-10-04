183eadd0f42615fba551347a235d3398312e517a
Роман Ермолаев <9868.08@gmail.com>
2021-10-04 22:27:30 +0400

    09_pedigree unwork

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -2,11 +2,18 @@ N = 4  # максимальная высота
 print('Введите количество человек: 9')
 # N = int(input('Введите количество человек: '))
 geno_set = []
-geno_dict = dict
-for i in range(1, N, 1):
+geno_dict = {}
+'''for i in range(1, N, 1):
 	print(i, 'пара', end=': ')
 	inc = input()
 	geno_set = inc.split()
-	print(geno_set)
-	geno_dict[i] = geno_set
-print(geno_dict)
+	geno_dict[i] = geno_set'''
+# атоматизация, чтобы не вводить код
+geno_dict = {1: ['aa_папа', 'bb_мама'], 2: ['cc_бабушка', 'dd_дедушка'], 3: ['bb_прабабушка',
+																			 'aa_прадедушка']}
+geno_dict_sorted = sorted(geno_dict)
+print('не сортированный список: ', geno_dict)
+print('сортированный список: ', geno_dict_sorted)
+list_geno = list(geno_dict.items())
+geno_sorted = list_geno.sort(key=lambda i: i[1])
+print(geno_sorted)
