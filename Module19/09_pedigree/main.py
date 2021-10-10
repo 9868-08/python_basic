92251ea344f4ae4ad1cd58da8a1a1cffe9e7026b
Роман Ермолаев <9868.08@gmail.com>
2021-10-10 21:38:15 +0400

    09_pedigree unwork

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,5 +1,17 @@
+def dict_max(dict_input):
+	for i in dict_input:
+		i_max = 0
+		index_max = 0
+		count = 0
+		if i_max<i:
+			i_max=i
+			index_max=count
+		count+=1
+	return (index_max)
+
+
 N = 4  # максимальная высота
-print('Введите количество человек: 9')
+print('Введите количество поколений: 4')
 # N = int(input('Введите количество человек: '))
 geno_set = []
 geno_dict = {}
@@ -9,11 +21,17 @@ geno_dict = {}
 	geno_set = inc.split()
 	geno_dict[i] = geno_set'''
 # атоматизация, чтобы не вводить код
-geno_dict = {1: ['aa_папа', 'bb_мама'], 2: ['cc_бабушка', 'dd_дедушка'], 3: ['bb_прабабушка',
+geno_dict = {1: ['ee_папа', 'ff_мама'], 2: ['cc_бабушка', 'dd_дедушка'], 3: ['bb_прабабушка',
 																			 'aa_прадедушка']}
-geno_dict_sorted = sorted(geno_dict)
 print('не сортированный список: ', geno_dict)
-print('сортированный список: ', geno_dict_sorted)
 list_geno = list(geno_dict.items())
 geno_sorted = list_geno.sort(key=lambda i: i[1])
 print(geno_sorted)
+
+for i_key in sorted(geno_dict):
+    print(i_key, geno_dict[i_key])
+
+list_keys = list(geno_dict.keys())
+list_keys.sort()
+for i in list_keys:
+    print(i, geno_dict.get(i))
