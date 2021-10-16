8f1be32baf96ef95be51e3098bbb9e2f3fc017a4
Роман Ермолаев <9868.08@gmail.com>
2021-10-16 13:52:44 +0400

    03_function release

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+input_set = ['aa','bb','cc','dd','bb','ee']
+inputed = 'bb'
+
+def f(f_set,f_inputed):
+	output_set = []
+	flag = False
+	for i in f_set:
+		if i == inputed and flag == False :
+			flag = True
+		elif i == inputed and flag == True :
+			flag = False
+		if flag == True:
+			output_set.append(i)
+
+	return output_set
+
+print(f(input_set,inputed))
