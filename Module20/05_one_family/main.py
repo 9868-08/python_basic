f4389068b4596bc7db6fbb7cda630ec65f835e1c
Роман Ермолаев <9868.08@gmail.com>
2021-10-16 22:01:10 +0400

    05_one_family release

--- Module20/05_one_family/main.py
+++ Module20/05_one_family/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+database = {'Сидоров Никита': 35, 'Сидорова Алина': 34, 'Сидоров Павел': 10, 'Топтунов Павел': 33}
+
+#searching = 'Сидоров'
+searching = input('Введите фамилию: ')
+for i in database:
+	for j in i.split():
+		if searching in j:
+			print(i,database[i])
