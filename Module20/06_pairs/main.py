cf64fe18b0f6a5a7166cb59dd013e9cc64be197c
Роман Ермолаев <9868.08@gmail.com>
2021-10-20 15:18:10 +0400

    06_pairs

--- Module20/06_pairs/main.py
+++ Module20/06_pairs/main.py
@@ -5,4 +5,14 @@ for i in inc:
 	if i%2 == 0:
 		temp = (inc[i],inc[i+1])
 		result.append(temp)
-print(result)
+print('первое решение',result)
+
+result1 = []
+for i in range(0,len(inc)):
+	result1.append((inc[i],inc[i+1]))
+	if i+3 < len(inc):
+		i += 1
+	else:
+		break
+print('второе решение',result)
+
