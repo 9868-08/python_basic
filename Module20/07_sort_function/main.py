e4766fabff61275a510d5af5e8c851b4e436fcd4
Роман Ермолаев <9868.08@gmail.com>
2021-10-21 22:54:28 +0400

    07_sort_function relaese

--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+def sort_func(inc_set):
+	for i in inc_set:
+		if i != int(i):
+			print('Список содержит не целое цисло')
+			return inc_set
+	return sorted(inc_set)
+
+inc = (1,9.1,2,8,3,7,3,6,4,5)
+print(sort_func(inc))
+
