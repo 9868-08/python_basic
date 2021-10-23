0778f8c7fa0756e572b906f72d0383dc5ba4f14c
Роман Ермолаев <9868.08@gmail.com>
2021-10-23 19:48:08 +0400

    09_competition_protocol unworks

--- Module20/09_competition_protocol/main.py
+++ Module20/09_competition_protocol/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+#records_count = input('Сколько записей вносится в протокол? ')
+records_count = 2
+database_dict=dict()
+
+print('Записи (результат и имя): ')
+'''for i in range (1, records_count):
+	print(i,' запись:', end=' ')
+	inc = input('')
+	database_dict[i] = inc.split()'''
+database_dict={1: ['69485', ' Jack'],
+			   2: ['95715', ' qwerty'],
+			   3: ['95715', 'Alex'],
+			   5: ['197128', 'qwerty'],
+			   6: ['95715', 'Jack'],
+			   7: ['93289', 'Alex']}
+
+for i_key in sorted(database_dict):
+	print(i_key, database_dict[i_key])
+
+list_keys = list(database_dict)
+list_keys.sort()
+for i in list_keys:
+    print(i, database_dict(i))
