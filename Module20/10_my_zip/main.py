62de687651a69f2ce90df64982cb9ed70c7947fb
Роман Ермолаев <9868.08@gmail.com>
2021-10-23 21:00:11 +0400

    10_my_zip ?

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+input_sring='abcd'
+input_set = [10, 20, 30, 40]
+result_set=[]
+for i in range(0,len(input_sring)):
+	result_set.append([input_sring[i], input_set[i]])
+
+print(result_set)
