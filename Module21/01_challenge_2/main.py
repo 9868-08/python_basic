7b11eb10fdfe8884f761130243ea94c314063ec1
Роман Ермолаев <9868.08@gmail.com>
2021-10-29 17:21:24 +0400

    01_challenge_2 relaes

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def my_func(num_finish):
+	num = 1
+	while num != num_finish+1:
+		print(num)
+		num += 1
+
+
+finish = int(input("Введите конечное число: "))
+my_func(finish)
