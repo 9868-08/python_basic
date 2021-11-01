56a99307e5dd0ab16797b353469927086a8dba45
Роман Ермолаев <9868.08@gmail.com>
2021-11-01 19:56:27 +0400

    05_make_function_faster release

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,9 +1,16 @@
 def calculating_math_func(data):
-    result = 1
-    for index in range(1, data + 1):
-        result *= index
-    result /= data ** 3
-    result = result ** 10
-    return result
+	factorial = dict()
+	global_factorial = dict()
+	result = 1
+	for index in range(1, data + 1):
+		factorial[index] = result
+		result *= index
+		global_factorial[index] = result
+	result /= data ** 3
+	result = result ** 10
+	print(factorial)
+	return result, global_factorial
 
-# TODO оптимизировать функцию
+
+global_factorial = dict
+print(calculating_math_func(5))
