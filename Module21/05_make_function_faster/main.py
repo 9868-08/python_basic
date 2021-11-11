1e421d96406b8b80bde84c6a5c6b4497eea3d6c8
Роман Ермолаев <9868.08@gmail.com>
2021-11-11 12:24:38 +0400

    05_make_function_faster release

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -8,16 +8,16 @@ def calculating_math_func(data):
 		global_factorial[index] = result
 	result /= data ** 3
 	result = result ** 10
-	print(factorial)
+#	print(factorial)
 	return result, global_factorial
 
+'''def add_elem(elem, data=[]):   # внимание на второй параметр
+    data.append(elem)
+    return data'''
 
 global_factorial = dict
 print(calculating_math_func(5))
 
-def add_elem(elem, data=[]):   # внимание на второй параметр
-    data.append(elem)
-    return data
-
-print(add_elem(10))
+'''print(add_elem(10))
 print(add_elem(20))
+'''
