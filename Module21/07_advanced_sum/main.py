6a0f682c2f934062c42fdeb7a00e8ff36b32b628
Роман Ермолаев <9868.08@gmail.com>
2021-11-13 12:43:39 +0400

    07_advanced_sum unworked

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def my_sum(input_list):
+	result = 0
+	for i_element in input_list:
+		if type(i_element) != list:
+			print('добавляется', i_element)
+			result += i_element
+		else:
+			for j_element in i_element:
+				print('добавляется', j_element)
+				result += j_element
+	return result
+
+
+my_sum([[1, 2, [3]], [1], 3])
