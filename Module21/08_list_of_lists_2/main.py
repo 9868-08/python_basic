46a9f54f2f45b0b7d94c0c89b164d74d79fe6e7d
Роман Ермолаев <9868.08@gmail.com>
2021-11-13 13:20:06 +0400

    08_list_of_lists_2 unworked

--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -1,4 +1,15 @@
+def list_exposure (list):
+    result=[]
+    for i_element in list:
+        if type(i_element) != list:
+            print('добавляется', i_element)
+            result.append(i_element)
+        else:
+            list_exposure(i_element)
+    return result
+
+
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
+print(list_exposure(nice_list))
 
-# TODO здесь писать код
