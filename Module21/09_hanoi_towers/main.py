f4c8a5685baaab44a17ca67ef269ea7359b3b56b
Роман Ермолаев <9868.08@gmail.com>
2021-11-13 14:14:59 +0400

    09_hanoi_towers template

--- Module21/09_hanoi_towers/main.py
+++ Module21/09_hanoi_towers/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+disk_count = int(input('Введите количество дисков: '))
+set_1=[]
+set_2=[]
+set_3=[]
+for i in range(1, disk_count+1):
+	set_1.append(i)
+print('Даны пирамиды', set_1,set_2,set_3)
+for i in range(1, disk_count+1):
+
+#	print('Переложить диск 1 со стержня номер', i, 'на стержень номер 2')
