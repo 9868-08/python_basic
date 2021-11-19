379836735b190dbbf8bb397ab818f811fdad548c
Роман Ермолаев <9868.08@gmail.com>
2021-11-19 11:07:48 +0400

    06_deep_copy unworks

--- Module21/06_deep_copy/main.py
+++ Module21/06_deep_copy/main.py
@@ -1,6 +1,18 @@
-def print_site(site,quantity,name):
-    print('Сайт для', name, ':\nsite = {')
-    print(site.format(name))
+def make_site(name):  # name - имя товара
+    # struct_site = copy.deepcopy(site)
+    struct_site = site.copy()  # надо будет импортировать copy
+
+    # ^ два варианта копирования, т.е. берём уже имеющуюся структуру, создаём копию её и закрепляем за переменной struct_site
+
+    new_title = 'Куплю/продам {} недорого'.format(name)  # новая строка
+
+    struct_site = change_value(struct_site, 'title',
+                               new_title)  # функция change_value будет идти по struct_site и заменять по ключу title строку на строку выше
+    new_h2 = 'У нас самая низкая цена на {}'.format(name)  # то же самое что и выше
+    struct_site = change_value(struct_site, 'h2', new_h2)
+
+    return struct_site  # делаем возврат
+
 
 site = {
     'html': {
@@ -18,4 +30,4 @@ site = {
 quantity = int(input('Количество сайтов: '))
 for i_site in range(0,quantity):
     name = input('Введите название продукта для нового сайта: ')
-    print_site(site,quantity,name)
+    print(make_site(name))
