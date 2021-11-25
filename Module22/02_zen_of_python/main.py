54e9c8f43fa5dd78c4f6c4b78a7d05a8529d1762
Роман Ермолаев <9868.08@gmail.com>
2021-11-25 12:51:06 +0400

    02_zen_of_python release

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+import os
+input_file = open('zen.txt', 'r')
+result = []
+for i_line in input_file:
+	result.append(i_line)
+
+out_file = open('answer.txt', 'w')
+for i in range(1, len(result)+1):
+	out_file.write(result[len(result)-i])
+	print('i=', i, result[len(result)-i])
+
+input_file.close()
+out_file.close()
