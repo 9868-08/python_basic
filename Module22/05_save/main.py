94c6949514bd940a7c3f5dc512c6215be934d4db
Роман Ермолаев <9868.08@gmail.com>
2021-11-28 22:02:43 +0400

    05_save unworks

--- Module22/05_save/main.py
+++ Module22/05_save/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+test_line = 'testiruyem'
+output_folder = '1 python_basic Module22 05_save'
+output_folder.split()
+print(output_folder)
+output_file = 'my_document'
+
+for i_item in output_folder:
+	print(i_item)
