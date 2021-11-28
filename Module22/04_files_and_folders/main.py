adc38c1bc6e15fe273e5bab9431d96fbefeb443b
Роман Ермолаев <9868.08@gmail.com>
2021-11-28 21:44:59 +0400

    04_files_and_folders release

--- Module22/04_files_and_folders/main.py
+++ Module22/04_files_and_folders/main.py
@@ -1,1 +1,15 @@
-input_path = input('Введите путь до каталога:')
+import os
+
+print("C:/1/python_basic/Module22/04_files_and_folders/1")
+dirname = input('Введите путь до каталога: ')
+dirfiles = os.listdir(dirname)
+
+fullpaths = map(lambda name: os.path.join(dirname, name), dirfiles)
+dirs = []
+files = []
+for file in fullpaths:
+    if os.path.isdir(file): dirs.append(file)
+    if os.path.isfile(file): files.append(file)
+
+print('В директории', dirname, len(list(dirs)), 'папок')
+print('В директории', dirname, len(list(files)), 'файлов')
