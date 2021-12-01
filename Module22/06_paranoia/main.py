00653a97af19d9379c61cd2b25c0c958efeb4fc5
Роман Ермолаев <9868.08@gmail.com>
2021-12-01 19:09:02 +0400

    06_paranoia in work

--- Module22/06_paranoia/main.py
+++ Module22/06_paranoia/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+def ceaser_cipher_def(message, k):
+	abc = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	count = 1
+	ceaser_cipher = ''
+	for letter in message:
+		print(letter)
+		if letter == " ":
+			ceaser_cipher += ' '
+		else:
+			index = abc.index(letter)
+			new_index = index + k
+			print('index=', index, 'abc[index]=', abc[index], 'abc[new_index]', abc[new_index])
+			if new_index > len(abc):
+				new_index = new_index - len(abc)
+			ceaser_cipher += abc[new_index]
+	print('Зашифрованное сообщение: ', ceaser_cipher)
+	return ceaser_cipher
+
+
+import os
+
+input_file = open('zen.txt', 'r')
+result = []
+k = 1  # начальный сдвиг
+for i_line in input_file:
+	result.append(ceaser_cipher_def(i_line, k))
+	k += 1
+input_file.close()
