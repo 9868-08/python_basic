619b876d00c4ec6fb81b5eedede4a187acc940ca
Роман Ермолаев <9868.08@gmail.com>
2021-12-01 19:53:09 +0400

    06_paranoia release

--- Module22/06_paranoia/main.py
+++ Module22/06_paranoia/main.py
@@ -1,28 +1,30 @@
 def ceaser_cipher_def(message, k):
 	abc = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-	count = 1
 	ceaser_cipher = ''
 	for letter in message:
-		print(letter)
 		if letter == " ":
 			ceaser_cipher += ' '
+		elif letter == "\n":
+			return ceaser_cipher
+		elif letter == ".":
+			ceaser_cipher += '.'
 		else:
 			index = abc.index(letter)
 			new_index = index + k
-			print('index=', index, 'abc[index]=', abc[index], 'abc[new_index]', abc[new_index])
+			#print('index=', index, 'abc[index]=', abc[index], 'abc[new_index]', abc[new_index])
 			if new_index > len(abc):
 				new_index = new_index - len(abc)
 			ceaser_cipher += abc[new_index]
-	print('Зашифрованное сообщение: ', ceaser_cipher)
 	return ceaser_cipher
 
 
-import os
-
-input_file = open('zen.txt', 'r')
-result = []
+input_file = open('text.txt', 'r')
+output_file = open('cipher_text.txt', 'a')
+result = ''
 k = 1  # начальный сдвиг
 for i_line in input_file:
-	result.append(ceaser_cipher_def(i_line, k))
+	result = ceaser_cipher_def(i_line, k)
+	result = result + '\n'
 	k += 1
+	output_file.write(result)
 input_file.close()
