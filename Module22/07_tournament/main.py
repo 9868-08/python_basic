611dce6b5fca29e9d5443db3d920429bdb629746
Роман Ермолаев <9868.08@gmail.com>
2021-12-02 15:17:55 +0400

    07_tournament in work

--- Module22/07_tournament/main.py
+++ Module22/07_tournament/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+input_file = open('first_tour.txt', 'r')
+output_file = open('second_tour.txt', 'a')
+result = ''
+k = 0
+result = dict()
+for i_line in input_file:
+	k += 1
+	i_line = i_line.split(' ')
+	print(i_line, i_line[0])
+	if k == 1:
+		continue
+#	result[int(i_line[2])] = '1'
+#	result = str(k-1) + ') ' + i_line
+print(result)
+#	output_file.write(result)
+input_file.close()
