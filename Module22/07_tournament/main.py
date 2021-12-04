db04d7702543353c6db6f9879b63d2f2a8a71dd9
Роман Ермолаев <9868.08@gmail.com>
2021-12-04 22:12:41 +0400

    07_tournament pre-release

--- Module22/07_tournament/main.py
+++ Module22/07_tournament/main.py
@@ -1,16 +1,25 @@
 input_file = open('first_tour.txt', 'r')
-output_file = open('second_tour.txt', 'a')
+output_file = open('second_tour.txt', 'w')
 result = ''
 k = 0
 result = dict()
 for i_line in input_file:
 	k += 1
 	i_line = i_line.split(' ')
-	print(i_line, i_line[0])
 	if k == 1:
 		continue
+	#	print(i_line, i_line[1][0])		#имя сокращено до 1 буквы
+	result[int(i_line[2])] = i_line[1][0] + '.' + i_line[0]
+
 #	result[int(i_line[2])] = '1'
 #	result = str(k-1) + ') ' + i_line
-print(result)
-#	output_file.write(result)
+print('1) V. Petrov 98')
+count = 1
+tmp = ""
+for i_key in sorted(result, reverse=True):
+	tmp = count, ')', result[i_key], i_key
+	print(tmp)
+	output_file.write(str(tmp))
+	output_file.write('\n')
+	count += 1
 input_file.close()
