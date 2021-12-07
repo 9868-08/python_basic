3f17a8131f63450f7c76ebe9a26953c05bfe4050
Роман Ермолаев <9868.08@gmail.com>
2021-12-07 15:39:46 +0400

    07_tournament pre-release

--- Module22/07_tournament/main.py
+++ Module22/07_tournament/main.py
@@ -1,3 +1,14 @@
+def sorted_dict (dict):
+	result_dict = dict()
+	i_value_max = 0
+	for i_key in dict:
+		if i_key > i_value_max:
+			i_value_max = i_key
+		print('i_key=', i_key, 'i_value=', dict[i_key] )
+		result_dict[i_key] = dict[i_key]
+	print(i_value_max, result_dict)
+	return
+
 input_file = open('first_tour.txt', 'r')
 output_file = open('second_tour.txt', 'w')
 result = ''
@@ -8,12 +19,8 @@ for i_line in input_file:
 	i_line = i_line.split(' ')
 	if k == 1:
 		continue
-	#	print(i_line, i_line[1][0])		#имя сокращено до 1 буквы
 	result[int(i_line[2])] = i_line[1][0] + '.' + i_line[0]
 
-#	result[int(i_line[2])] = '1'
-#	result = str(k-1) + ') ' + i_line
-print('1) V. Petrov 98')
 count = 1
 tmp = ""
 for i_key in sorted(result, reverse=True):
@@ -22,4 +29,5 @@ for i_key in sorted(result, reverse=True):
 	output_file.write(str(tmp))
 	output_file.write('\n')
 	count += 1
+print(sorted_dict(result))
 input_file.close()
--- Module22/08_frequency_analysis/main.py
+++ Module22/08_frequency_analysis/main.py
@@ -1,1 +1,66 @@
-# TODO здесь писать код
+import os
+
+
+def analyse_line(letters_dict, line):
+    char_count = 0
+    for char in line.lower():
+        if 'a' <= char <= 'z':
+            if char not in letters_dict:
+                letters_dict[char] = 0
+            letters_dict[char] += 1
+            char_count += 1
+    words_count = len(line.split(' '))
+#    print(letters_dict)
+    return char_count, words_count
+
+
+def get_file_path(file_name):
+    return os.path.abspath(os.path.join('.', file_name))
+
+
+def get_lines(file_name):
+    file_path = get_file_path(file_name)
+    out = []
+    with open(file_path, 'r') as input_file:
+        out += input_file.readlines()[::-1]
+
+    return out
+
+
+def output(char_count, words_count, lines_count, least_freq_char, letters_dict):
+    print("Количество букв в файле:", char_count)
+    print("Количество слов в файле:", words_count)
+    print("Количество строк в файле:", lines_count)
+    print("Наиболее редкая буква:", least_freq_char)
+    for i_index in letters_dict:
+        print(i_index)
+    print(letters_dict, len(letters_dict))
+
+
+
+def get_min_freq_char(items):
+    min_freq = 0
+    min_freq_char = ''
+    for item in items:
+        if item[1] < min_freq:
+            min_freq = item[1]
+            min_freq_char = item[0]
+    return min_freq_char
+
+
+def main(input_filename):
+    lines_count = 0
+    char_count = 0
+    words_count = 0
+    letters_dict = dict()
+    for line in get_lines(input_filename):
+        chars_in_line, words_in_line = analyse_line(letters_dict, line)
+        char_count += chars_in_line
+        words_count += words_in_line
+        lines_count += 1
+
+    least_freq_char = get_min_freq_char(letters_dict.items())
+    output(char_count, words_count, lines_count, least_freq_char, letters_dict)
+
+
+main('zen.txt')
