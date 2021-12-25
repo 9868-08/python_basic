00c2ce82559fa2abd5829ec013ae864ca637f83f
Roma Ermolaev <9868.08@gmail.com>
2021-12-25 21:47:32 +0400

    09_war_and_peace un-works

--- Module22/09_war_and_peace/main.py
+++ Module22/09_war_and_peace/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+import csv, io, sys, zipfile
+
+zip_file = zipfile.ZipFile('voyna-i-mir.zip')
+items_file  = zip_file.open('voyna-i-mir.txt', 'r')
+# items_file.readable = lambda: True
+# items_file.writable = lambda: False
+# items_file.seekable = lambda: False
+# items_file.read1 = items_file.read
+items_file  = io.TextIOWrapper(items_file)
+
+for idx, row in enumerate(csv.DictReader(items_file)):
+    print('Processing row {0} -- row = {1}'.format(idx, row))
+
+
