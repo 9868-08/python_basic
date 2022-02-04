9d58ee6f012b2a5885205bb3841341c830f17378
9868.08@gmail.com <Zv6KMMhY>
2022-02-04 23:36:51 +0400

    02_students pre-release

--- Module24/02_students/main.py
+++ Module24/02_students/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+#Реализуйте модель с именем Student, содержащую поля: «ФИ», «Номер группы», «Успеваемость» (список из пяти элементов).
+#Затем создайте список из десяти студентов (данные о студентах можете придумать свои либо запросить их у пользователя) и
+#отсортируйте его по возрастанию среднего балла. Выведите результат на экран.
+
+from random import randint
+
+class Student:
+    ФИ = str
+    group = str
+    progress = set()
+
+group = input('Заполняется ведомость группы: ')
+for i_num in range (1,11):
+    print('ФИО', i_num, 'студента: ', end='')
+    Student.ФИ=input()
+    Student.group = group
+    for j_num in range(0, 5):
+        progress = randint(2, 5)
+#        print(randint(2, 5))
+        Student.progress.add(progress)
+    print(Student.progress)
