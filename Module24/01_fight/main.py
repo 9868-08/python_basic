a1f180ed88d7507d57fd407ba7d3fc8f400ff130
9868.08@gmail.com <Zv6KMMhY>
2022-02-04 23:16:59 +0400

    01_fight release

--- Module24/01_fight/main.py
+++ Module24/01_fight/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+from random import randint
+
+health_unit_1 = 100
+health_unit_2 = 100
+
+i = 1
+while health_unit_1 > 10 and health_unit_2:
+    if randint(0, 1) == 0:
+        health_unit_1 -= randint(1, 20)
+        print('атаковал 1 воин, у воина 2 осталось', health_unit_2, 'здоровья')
+    else:
+        health_unit_2 -= randint(1, 20)
+        print('атаковал 2 воин, у воина 1 осталось', health_unit_1, 'здоровья')
+
+print(health_unit_1, health_unit_2)
+if health_unit_1 > health_unit_2:
+    print('победу одержал 1 воин')
+else:
+    print('победу одержал 2 воин')
