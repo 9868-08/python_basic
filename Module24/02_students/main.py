5e94f91118ade869bf9fcfd451d1e0dafa7ba360
9868.08@gmail.com <Zv6KMMhY>
2022-02-07 15:54:08 +0400

    03_circle missing 1 required positional argument: 'self'

--- Module24/02_students/main.py
+++ Module24/02_students/main.py
@@ -1,21 +1,30 @@
-#Реализуйте модель с именем Student, содержащую поля: «ФИ», «Номер группы», «Успеваемость» (список из пяти элементов).
-#Затем создайте список из десяти студентов (данные о студентах можете придумать свои либо запросить их у пользователя) и
-#отсортируйте его по возрастанию среднего балла. Выведите результат на экран.
+# Реализуйте модель с именем Student, содержащую поля: «ФИ», «Номер группы», «Успеваемость» (список из пяти элементов).
+# Затем создайте список из десяти студентов (данные о студентах можете придумать свои либо запросить их у пользователя) и
+# отсортируйте его по возрастанию среднего балла. Выведите результат на экран.
 
 from random import randint
 
+
 class Student:
-    ФИ = str
-    group = str
+    ФИ = str()
+    group = str()
     progress = set()
 
-group = input('Заполняется ведомость группы: ')
-for i_num in range (1,11):
-    print('ФИО', i_num, 'студента: ', end='')
-    Student.ФИ=input()
-    Student.group = group
+    def info(self):
+        print('{}   {}   {}'.format(self.ФИ, self.group, self.progress))
+
+
+current_student = Student()  # экземпляр класса Student
+# group = input('Заполняется ведомость группы: ')
+group = "Группа-1"
+students_list = set()
+for i_num in range(0, 10):
+    current_student.ФИ = "Иванов Иван Иваныч" + str(- i_num)
+    current_student.group = group
     for j_num in range(0, 5):
         progress = randint(2, 5)
-#        print(randint(2, 5))
+        print('progress=', progress)
         Student.progress.add(progress)
-    print(Student.progress)
+    students_list.add(current_student)
+
+    print(students_list.pop().info())
--- Module24/03_circle/main.py
+++ Module24/03_circle/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+import math
+
+class Circle:
+    x = 0
+    y = 0
+    R = 1
+    def square(self):
+        print(math.pi * self.R ^ 2)
+
+s=Circle
+s.square()
