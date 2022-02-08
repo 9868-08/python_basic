f93b6a295d1c0f7d91aabe5b4a6d16ba4909bbd7
9868.08@gmail.com <Zv6KMMhY>
2022-02-08 16:52:00 +0400

    03_circle

--- Module24/03_circle/main.py
+++ Module24/03_circle/main.py
@@ -1,11 +1,17 @@
 import math
 
-class Circle:
-    x = 0
-    y = 0
-    R = 1
-    def square(self):
-        print(math.pi * self.R ^ 2)
 
-s=Circle
-s.square()
+class Circle:
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.R = 1
+
+    def square(self):
+        print(math.pi * self.R ** 2)
+        return
+
+
+test_circle = Circle()
+test_circle.square()
--- Module24/04_fathers_mothers_kids/main.py
+++ Module24/04_fathers_mothers_kids/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+class Parent:
+    name = "имя родителя"
+    aged = 20
+    tasks = ['купить еды', 'приготовить обед']
+    def info(self):
+        print('меня зовут {}/nМне {} лет/n дела {}', format(self.name, self.aged,self.tasks))
+class child:
+    name = str
+    aged = int
+
+me=Parent
+me.info(self=)
