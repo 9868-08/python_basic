38151968ebfd4a9bff569ab212dc924bc20f304c
9868.08@gmail.com <Mis8FhXJ>
2022-02-09 17:13:55 +0400

    02_students sorting unworks

--- Module24/02_students/main.py
+++ Module24/02_students/main.py
@@ -1,30 +1,49 @@
-# Реализуйте модель с именем Student, содержащую поля: «ФИ», «Номер группы», «Успеваемость» (список из пяти элементов).
-# Затем создайте список из десяти студентов (данные о студентах можете придумать свои либо запросить их у пользователя) и
-# отсортируйте его по возрастанию среднего балла. Выведите результат на экран.
 
 from random import randint
 
 
 class Student:
-    ФИ = str()
+    FI = str()
     group = str()
-    progress = set()
+    progress = list()
+    average = 0
 
     def info(self):
-        print('{}   {}   {}'.format(self.ФИ, self.group, self.progress))
+        print('{}   {}   {} {}'.format(self.FI, self.group, self.progress, self.average))
 
 
-current_student = Student()  # экземпляр класса Student
+def student_sort(student_sort_list):
+    def_max = 0
+    student_def = list()
+    for j_student in student_sort_list:
+        for i_student in student_sort_list:
+            if i_student.average > def_max and i_student != j_student:
+                def_max = i_student.average
+                student_def.append(i_student)
+            def_max = 0
+    return student_def
+
+
 # group = input('Заполняется ведомость группы: ')
 group = "Группа-1"
-students_list = set()
-for i_num in range(0, 10):
-    current_student.ФИ = "Иванов Иван Иваныч" + str(- i_num)
-    current_student.group = group
-    for j_num in range(0, 5):
-        progress = randint(2, 5)
-        print('progress=', progress)
-        Student.progress.add(progress)
-    students_list.add(current_student)
+students_list = list()
+# progress = list()
 
-    print(students_list.pop().info())
+for i_num in range(0, 10):
+    current_student = Student()  # экземпляр класса Student
+    current_student.progress = []
+    current_student.FI = "Иванов Иван Иваныч " + str(- i_num) + "-й"
+    current_student.group = group
+    average = 0
+    for j_num in range(0, 5):
+        current_student.progres = []
+        progress = randint(2, 5)
+        current_student.progress.append(progress)
+        average += progress
+    current_student.average = average / 5
+    students_list.append(current_student)
+    current_student.info()
+sorted_students  = student_sort(students_list)
+
+for i_student in sorted_students:
+    i_student.info()
