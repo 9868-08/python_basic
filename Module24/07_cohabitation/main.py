0ff89637731248b92b0f1bdfd76073b027b32cd2
Roman <9868.08@gmail.com>
2022-02-23 22:36:41 +0400

    07_cohabitation unworks

--- Module24/07_cohabitation/main.py
+++ Module24/07_cohabitation/main.py
@@ -2,23 +2,23 @@ from random import randrange
 
 
 class Man:
-    hungry = 50
+    satiety = 50            # сытость (вместо hungry)
 
-    def __init__(self, name="Имя по-умолчанию", hungry=50):
+    def __init__(self, name="Имя по-умолчанию", satiety=50):
         self.name = name
-        self.hungry = hungry
+        self.satiety = satiety
         self.house = House()
 
     def info(self):
-        print('сосед {} голодный: {} '.format(self.name, self.hungry))
+        print('сосед {} голодный: {} '.format(self.name, self.satiety))
 
-    def def_eat(self, def_man):
-        def_man.hungry += 10
-        def_man.house.fridge -= 10
-        return def_man
+    def def_eat(self):
+        self.satiety += 10
+        self.house.fridge -= 10
+        return
 
     def def_work(self, def_man):
-        def_man.hungry -= 10
+        def_man.satiety -= 10
         def_man.house.nightstand_with_money += 10
         return def_man
 
@@ -28,18 +28,18 @@ class Man:
         return def_man
 
     def def_game(self, def_man):
-        def_man.hungry -= 10
+        def_man.satiety -= 10
         return def_man
 
     def def_action(self, def_man, def_action):
-        if def_man.hungry<0:
+        if def_man.satiety<0:
             print("{} умер".format(def_man.name))
         if def_action == 1:
             print("на {} день они пошел работать".format(day))
             def_man = def_man.def_work(def_man)
         elif action == 2:
             print("на {} день {} поел".format(day, def_man.name))
-            def_man = def_man.def_eat(def_man)
+            def_man = def_man.def_eat()
 
         return def_man
 
@@ -66,15 +66,14 @@ neighbor1.house = my_house
 neighbor2.house = my_house
 house_residents = [neighbor1, neighbor2]
 '''
-
 day = 1
 while day < 365:
     for resident in house_residents:
         action = (randrange(6))
         #    print("action=", action)
-        if resident.hungry < 20:
+        if resident.satiety < 20:
             print("на {} день {} покушал, так как проголодался".format(day, neighbor1.name))
-            resident = resident.def_eat(resident)
+            resident = resident.def_eat()
         elif my_house.fridge < 10:
             print("на {} день {} пошел в магазин".format(day,resident.name))
             resident = resident.def_buy(resident)
@@ -84,7 +83,7 @@ while day < 365:
         elif action == 1:
             resident = resident.def_work(resident)
         elif action == 2:
-            resident = resident.def_eat(resident)
+            resident = resident.def_eat()
         else:
             print("на {} день они играли".format(day))
             neighbor1 = neighbor1.def_game(neighbor1)
