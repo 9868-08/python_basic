4faf42d4967fe47933a20c57c7757d9b679afdec
Roman <9868.08@gmail.com>
2022-02-23 22:20:10 +0400

    07_cohabitation unworks

--- Module24/09_tic-tac-toe/main.py
+++ Module24/09_tic-tac-toe/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+class Field():
+    def __init__(self, x_max, y_max):
+        result = list[0][0]
+        for i in range(1, x_max):
+            for j in range(1, y_max):
+                result.append[[i], [j]] = 0
+        return result
+
+x_max = 3
+y_max = 3
+my_game = Field(x_max, y_max)
+print(my_game)
