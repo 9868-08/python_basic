70a4bef1ccb23351b4ee3232df93f17dd9d0667f
Roman <9868.08@gmail.com>
2022-03-12 15:30:00 +0400

    08_blackjack: class Deck

--- Module24/08_blackjack/main.py
+++ Module24/08_blackjack/main.py
@@ -1,43 +1,38 @@
-class Cards:
-    card_values = {"2": 2,
-                   "3": 3,
-                   "4": 4,
-                   "5": 5,
-                   "6": 6,
-                   "7": 7,
-                   "8": 8,
-                   "9": 9,
-                   "10": 10,
-                   "11": 10,  # король
-                   "12": 10,  # королева
-                   "13 ": 10,  # валет
-                   "14": 11}  # туз
+# Задаем "константы" для выявления карт.
+NUM_CARDS = [str(n) for n in range(2, 11)]
+PICT_CARDS = ['Валет', 'Дама', 'Король', 'Туз']
+MAX_SUM = 21
 
+
+# Всего там стоит сделать ряд классов: Deck, Card и Dealer
+
+
+class Deck:
     def __init__(self):
+        self.card_list = NUM_CARDS + PICT_CARDS
         return
 
-    def summ(self, cards_list):
-        def_sum = 0
-        for i_card in cards_list:
-            if i_card % 10 == 0:
-                def_sum += i_card
-            elif i_card == 14 and def_sum < 21:
-                def_sum += 11
-            elif i_card == 14 and def_sum > 21:
-                def_sum += 1
-            print("текущая карта", i_card)
-        return def_sum
+    def __add__(self, right):
+        # для "мёрджа" двух деков, или дека и карты.
+        return
 
+    def append(self, card):
+        self.append(card)
+        # добавляем карту к деку, достаточно просто, тот же аппенд к card_list
 
-from random import randrange
+    def get_cost(self):
+        return
+        # сумма карт. Идём по массиву вложенному в объект и согласно константам определённым выше вычисляем всю "стоимость"
 
-my_cards_list = []
-new_card = Cards()
-for i_card in range(1, 3):
-    new
-    my_cards_list.append(randrange(14))
-#    temp = randrange(14)
-#    temp = 14
-#    print(i_card, temp, card_valuses.get(str(temp)))
-print(my_cards_list)
-my_cards_list.su
+    def __len__(self):
+        return len(self.card_list)
+
+    def is_over(self):
+        return self.get_cost() > MAX_SUM
+
+    def info(self):
+        for i in self.card_list:
+            print(i)
+
+my_deck = Deck()
+my_deck.info()
--- Module24/09_tic-tac-toe/main.py
+++ Module24/09_tic-tac-toe/main.py
@@ -1,12 +1,24 @@
-class Field():
-    def __init__(self, x_max, y_max):
-        result = list[0][0]
-        for i in range(1, x_max):
-            for j in range(1, y_max):
-                result.append[[i], [j]] = 0
-        return result
+class Field:
+    #    satiety = 50            # сытость (вместо hungry)
 
-x_max = 3
-y_max = 3
-my_game = Field(x_max, y_max)
-print(my_game)
+    def __init__(self, size=3):
+        self.tmp = []
+        self.result = []
+        for i in range(0, size):
+            self.tmp.append(0)
+        for i in range(0, size):
+            self.result.append(self.tmp)
+        return
+
+    def info(self):
+        print('сосед {}  '.format(self.result))
+
+
+my_field = Field(3)
+my_field.info()
+
+while True:
+    raw_input = input("ваш ход: ")
+    input_x, input_y = raw_input().split(' ')
+
+    print(input_x, input_y)
