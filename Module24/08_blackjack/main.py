5ab1c0f362748d2f831f374e08b503757bd3ec5f
Roman <9868.08@gmail.com>
2022-03-19 13:38:16 +0400

    08_blackjack: release

--- Module24/08_blackjack/main.py
+++ Module24/08_blackjack/main.py
@@ -19,18 +19,24 @@ class Deck:  # вся колода карт
         self.append(card)
         # добавляем карту к деку, достаточно просто, тот же аппенд к card_list
 
+    def info(self):
+        count = 0
+        for i in self.card_list:
+            print(i, count)
+            count +=1
+
 
 # сумма карт. Идём по массиву вложенному в объект и согласно константам определённым выше вычисляем всю "стоимость"
 def get_cost(cards):
     cost = 0
-    for i in cards:
-        if i in NUM_CARDS:
-            cost += int(i)
-        elif i == 'Валет' or i == 'Дама' or i == 'Король':
+    for item in cards:
+        if item in NUM_CARDS:
+            cost += int(item)
+        elif item == 'Валет' or item == 'Дама' or item == 'Король':
             cost += 10
-        elif i == 'Туз' and cost < 21:
+        elif item == 'Туз' and cost < 21:
             cost += 11
-        elif i == 'Туз' and cost > 21:
+        elif item == 'Туз' and cost > 21:
             cost += 1
 
     return cost
@@ -48,9 +54,20 @@ def get_cost(cards):
 
 full_deck = Deck()  # колода одномастных карт
 # карты себе
-my_deck = [str(full_deck.card_list[randint(0, len(full_deck.card_list))]) for i in range(0, 2)]
-print("мне выданы:   ", my_deck, "количество очков: ", get_cost(my_deck))
-
+tmp = randint(0, len(full_deck.card_list) - 1)
+my_deck = [str(full_deck.card_list[tmp]) for i in range(0, 1)]
 # карты дилеру
-dealer_deck = [str(full_deck.card_list[randint(0, len(full_deck.card_list))]) for i in range(0, 2)]
-print("крупье выданы:", dealer_deck, "количество очков: ", get_cost(dealer_deck))
+tmp = randint(0, len(full_deck.card_list) - 1)
+dealer_deck = [str(full_deck.card_list[tmp]) for i in range(0, 1)]
+answer = 0
+while answer == 0:
+    # карты себе
+    tmp = randint(0, len(full_deck.card_list)-1)
+    my_deck.append(str(full_deck.card_list[tmp]))
+    print("мне выданы:   ", my_deck, "количество очков: ", get_cost(my_deck))
+
+    # карты дилеру
+    tmp = randint(0, len(full_deck.card_list)-1)
+    dealer_deck.append(str(full_deck.card_list[tmp]))
+    print("крупье выданы:   ", dealer_deck, "количество очков: ", get_cost(dealer_deck))
+    answer = int(input("сдать еще? (0 - еще, не 0 - хватит): "))
