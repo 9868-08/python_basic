f06f23f694a606884c454a331b206a52eb734bcc
Roman <9868.08@gmail.com>
2022-04-01 16:59:26 +0400

    02_karma: pre-release

--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -1,90 +1,54 @@
-import random
+from random import randint
 
 
+# define Python user-defined exceptions
 class TestFailed(Exception):
-    def __init__(self):
-        self.message = "TestFailed"
+    pass
 
-    def __str__(self):
-        return self.message
+    def __init__(self):
+        print('TestFailed exception')
+        file = open('karma.log', 'w')
+        file.write('TestFailed exception')
+        return
 
 
 class KillError(Exception):
+    pass
+
+
+class DrunkError:
+    pass
+
+
+class CarCrashError:
+    pass
+
+
+class GluttonyError:
+    pass
+
+
+class DepressionError:
+    pass
+
+
+class Life:
+
     def __init__(self):
-        self.message = "KillError"
-
-    def __str__(self):
-        return self.message
-
-
-class DrunkError(Exception):
-    def __init__(self):
-        self.message = "DrunkError"
-
-    def __str__(self):
-        return self.message
+        self.target_karma = 500
+        self.karma = 0
+        self.exception_list = [KillError, DrunkError, CarCrashError, GluttonyError, DepressionError]
 
 
-class CarCrashError(Exception):
-    def __init__(self):
-        self.message = "CarCrashError"
-
-    def __str__(self):
-        return self.message
-
-
-class GluttonyError(Exception):
-    def __init__(self):
-        self.message = "GluttonyError"
-
-    def __str__(self):
-        return self.message
-
-
-class DepressionError(Exception):
-    def __init__(self):
-        self.message = "DepressionError"
-
-    def __str__(self):
-        return self.message
-
-
-errors = [KillError, DrunkError, CarCrashError, GluttonyError, DepressionError]
-err = []
-try:
-    if random.randint(1, 10) == 1:
-        raise KillError('KillError')
-except (KillError("1"), DrunkError, CarCrashError, GluttonyError) as e:
-    with open('karma.log', 'r') as file:
-        e = file.readlines()
-    err.append((random.choice([KillError, DrunkError, CarCrashError, GluttonyError]) + '\n'))
-    with open('karma.log', 'w') as file:
-        for i in err:
-            file.write(str(i))
-#print(random.randint(1, 7))
-
-
-def one_day():
-    errors = [KillError, DrunkError, CarCrashError, GluttonyError]
-    err = []
+my_life = Life()
+while my_life.karma < my_life.target_karma:
     try:
-        if random.randint(1, 10) == 1:
-            my_error = random.choice(errors)
-            print(my_error.__str__)
-            raise my_error
+        fate = randint(0, 10)
+        if fate == 0:
+            raise TestFailed()
+        else:
+            my_life.karma += fate
+    finally:
+#        print(my_life.karma)
+        print("my_life.karma")
 
-    except (KillError, DrunkError, CarCrashError, GluttonyError) as e:
-        with open('karma.log', 'r') as file:
-            e = file.readlines()
-        err.append((str(random.choice([KillError, DrunkError, CarCrashError, GluttonyError])) + '\n'))
-        with open('karma.log', 'w') as file:
-            for i in err:
-                file.write(str(i))
-    return random.randint(1, 7)
-
-
-KARMA = 500
-life_karma = 0
-while life_karma <= KARMA:
-    life_karma += one_day()
-print(life_karma)
--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+class MyDict:
+    def __init__(self, incoming_dict):
+        if incoming_dict
+        self.my_dict = incoming_dict
+
+
+
+
+
+    
