b0ad929878840e0b259757a3b1329292c5e035c8
Roman <9868.08@gmail.com>
2022-04-04 11:33:34 +0400

    02_karma: release

--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -2,35 +2,59 @@ from random import randint
 
 
 # define Python user-defined exceptions
-class TestFailed(Exception):
+class KillError(Exception):
     pass
 
     def __init__(self):
-        print('TestFailed exception')
-        file = open('karma.log', 'w')
-        file.write('TestFailed exception')
+#        print('KillError exception')
+        file = open('karma.log', 'a')
+        file.write('KillError exception\n')
         file.close()
         return
 
 
-class KillError(Exception):
+class DrunkError(Exception):
     pass
 
+    def __init__(self):
+#        print('DrunkError exception')
+        file = open('karma.log', 'a')
+        file.write('DrunkError exception\n')
+        file.close()
+        return
 
-class DrunkError:
+
+class CarCrashError(Exception):
     pass
 
+    def __init__(self):
+#        print('CarCrashError exception')
+        file = open('karma.log', 'a')
+        file.write('CarCrashError exception\n')
+        file.close()
+        return
 
-class CarCrashError:
+
+class GluttonyError(Exception):
     pass
 
+    def __init__(self):
+#        print('GluttonyError exception')
+        file = open('karma.log', 'a')
+        file.write('GluttonyError exception\n')
+        file.close()
+        return
 
-class GluttonyError:
+
+class DepressionError(Exception):
     pass
 
-
-class DepressionError:
-    pass
+    def __init__(self):
+#        print('DepressionError exception')
+        file = open('karma.log', 'a')
+        file.write('DepressionError exception\n')
+        file.close()
+        return
 
 
 class Life:
@@ -38,23 +62,50 @@ class Life:
     def __init__(self):
         self.target_karma = 500
         self.karma = 0
-#        self.exception_list = [KillError, DrunkError, CarCrashError, GluttonyError, DepressionError]
 
     def __str__(self):
         return self.karma
 
 
+import os.path
+
+log_file = "karma.log"
+if os.path.exists(log_file):
+    os.remove(log_file)
+
+# Проверяем если папка существует.
+os.path.exists('storage')  # True
 my_life = Life()
 while my_life.karma < my_life.target_karma:
     try:
         fate = randint(0, 10)
         if fate == 0:
-            raise TestFailed()
+            raise KillError
+        elif fate == 1:
+            raise DrunkError
+        elif fate == 2:
+            raise CarCrashError
+        elif fate == 3:
+            raise GluttonyError
+        elif fate == 4:
+            raise DepressionError
         else:
             my_life.karma += fate
+
+    except KillError:
+        pass
+    except DrunkError:
+        pass
+    except CarCrashError:
+        pass
+    except GluttonyError:
+        pass
+    except DepressionError:
+        pass
+
+
+
     finally:
         pass
-#        print(my_life.karma)
-#        print("my_life.karma")
-print(my_life.__str__())
 
+print("достигнута карма: ", my_life.__str__())
