accc98f58fc9d5d386470885b1ba05880d48e0e8
Roman <9868.08@gmail.com>
2022-04-12 09:32:18 +0400

    1

--- Module26/01_num_squares/main.py
+++ Module26/01_num_squares/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+""" N = 0
+while N == 0:
+    try:
+        N = int(input("введите число N: "))
+    except ValueError:
+        print("нужно целое ввести число (тип int)")
+"""
+
+N = 10
+for i_counter in range(1, N+1):
+    print(i_counter, "\t: ", i_counter **2 )
