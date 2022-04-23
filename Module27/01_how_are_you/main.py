f8d910d7fbbc4e5b414c59d7a547c91070e2a8be
Roman Ermolaev <erm3534@ya.ru>
2022-04-23 11:20:26 +0400

    01_how_are_you release

--- Module27/01_how_are_you/main.py
+++ Module27/01_how_are_you/main.py
@@ -1,2 +1,19 @@
-def 1:
-    pass
+from functools import wraps
+
+
+def how_are_you(fn):
+    @wraps(fn)
+    def wrapped():
+        input("как дела? ")
+        print("А у меня не очень! Ладно, держи свою функцию.")
+        return fn()
+
+    return wrapped
+
+
+@how_are_you
+def test():
+    return "Тут что-то происходит...>"
+
+
+print(test())
