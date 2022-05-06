7c94af17212b7147b7054656200ac46ff38c37c3
Roman Ermolaev <erm3534@ya.ru>
2022-05-06 10:37:20 +0400

    04_date in work

--- Module28/04_date/main.py
+++ Module28/04_date/main.py
@@ -1,1 +1,32 @@
-# TODO здесь писать код
+class Date:
+    def __init__(self):
+        self.year = None
+        self.month = None
+        self.day = None
+
+    def __str__(self):
+        return "День: " + str(self.day) +"	Месяц: " + str(self.month) + "	Год: " + str(self.year)
+
+    def from_string(self, input):
+        x = input.split("-")
+        if int(x[0]) <= 31:
+            self.month = int(x[0])
+        else:
+            print(x[0], "Ошибка - день не может быть больше 31")
+
+        self.day = x[0]
+        if int(x[1]) <= 12:
+            self.month = int(x[1])
+        else:
+            print(x[1], "Ошибка - месяц не может быть больше 12")
+        self.year = x[2]
+
+
+my_date = Date()
+my_date.from_string('32-12-2077')
+print(my_date)
+
+date = Date.from_string('10-12-2077')
+print(date)
+print(Date.is_date_valid('10-12-2077'))
+print(Date.is_date_valid('40-12-2077'))
