22eed766ab917099ca497c2d0afeec69e313329e
Roman Ermolaev <erm3534@ya.ru>
2022-05-13 19:24:29 +0400

    03_format_logging unworked

--- Module29/03_format_logging/main.py
+++ Module29/03_format_logging/main.py
@@ -1,1 +1,35 @@
-# TODO здесь писать код
+def log_methods(def_a):
+    return
+
+
+@log_methods("b d Y - H:M:S")
+class Aa:
+    def test_sum_1(self) -> int:
+        print('test sum 1')
+        number = 100
+        result = 0
+        for _ in range(number + 1):
+            result += sum([i_num ** 2 for i_num in range(10000)])
+
+        return result
+
+
+@log_methods("b d Y - H:M:S")
+class Bb(Aa):
+    def test_sum_1(self):
+        super().test_sum_1()
+        print("Наследник test sum 1")
+
+    def test_sum_2(self):
+        print("test sum 2")
+        number = 200
+        result = 0
+        for _ in range(number + 1):
+            result += sum([i_num ** 2 for i_num in range(10000)])
+
+        return result
+
+
+my_obj = Bb()
+my_obj.test_sum_1()
+my_obj.test_sum_2()
