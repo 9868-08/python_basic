47c3ea070ad791b1cb2c35e403c57695a941feb0
Roman Ermolaev <erm3534@ya.ru>
2022-05-15 23:05:00 +0400

    04_decorated_world сделано как в решении: 30.1 Разбор домашнего задания

--- Module29/04_decorated_world/main.py
+++ Module29/04_decorated_world/main.py
@@ -1,44 +1,33 @@
 #Реализуйте декоратор для декораторов: он должен декорировать другую функцию, которая
 #должна быть декоратором, и даёт возможность любому декоратору принимать произвольные
 # аргументы.
-# decorator_with_args_for_any_decorator это декоратор для декораторов. Это решение из
-# Module29\01_permissions подходит для решения задачи
 
 import functools
-from functools import wraps
 from collections.abc import Callable
 
 
-def decorator_with_args_for_any_decorator(input_user: str = None) -> Callable:
-    def decorated_decorator(fn: Callable) -> Callable:
-
-        @functools.wraps(fn)
-        def wrapped():
-            users = ['admin', 'user']
-            if input_user in users:
-                print("доступ для выполнения {} разрешен".format(fn))
-            else:
-                print("доступ для выполнения {} запрещен".format(fn))
-                raise PermissionError
-
-            return fn()
-        return wrapped
-    return decorated_decorator
+def decorator_with_args_for_any_decorator(decorator_to_enchance: Callable) -> Callable:
+    """ Декоратор дает возможность другому декоратору принимать произвольные аргументы """
+    def decorator_maker(*args, **kwargs) -> Callable:
+        def decorator_wrapper(func: Callable) -> Callable:
+            return decorator_to_enchance(func, *args, **kwargs)
+        return decorator_wrapper
+    return decorator_maker
 
 
-@decorator_with_args_for_any_decorator('admin')
-def delete_site(input_user=None):
-    print('Удаляем сайт')
-    return
+@decorator_with_args_for_any_decorator
+def decorated_decorator(func: Callable, *args, **kwargs) -> Callable:
+    """" Декоратор шаблон """
+    @functools.wraps(func)
+    def wrapper(*func_args, **func_kwargs) -> Callable:
+        print("Переданные арги в кварги в декоратор: ", dec_args, dec_kwrgs)
+        return func(*func_args, **func_kwargs)
+    return wrapper
 
 
-@decorator_with_args_for_any_decorator('user_1')
-def add_comment(input_user=None):
-    print('Добавляем комментарий')
+@decorated_decorator(100, 'рублей', 200, 'друзей')
+def decorated_function(text: str, num: int) -> None:
+    print("Привет", text, num)
 
 
-delete_site()
-try:
-    add_comment()
-except PermissionError:
-    print("У пользователя нет прав на это действие")
+decorated_function("Юзер", 101)
