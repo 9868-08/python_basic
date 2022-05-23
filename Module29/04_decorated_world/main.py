45a1d2ec62278e212308eff9f36f8a42a0f5db05
Roman Ermolaev <erm3534@ya.ru>
2022-05-23 22:23:15 +0400

    04_decorated_world release (teacher solution)

--- Module29/04_decorated_world/main.py
+++ Module29/04_decorated_world/main.py
@@ -2,31 +2,31 @@
 #должна быть декоратором, и даёт возможность любому декоратору принимать произвольные
 # аргументы.
 
+from typing import Callable, Optional, Any
 import functools
-from collections.abc import Callable
 
-
-def decorator_with_args_for_any_decorator(decorator_to_enchance: Callable) -> Callable:
-    """ Декоратор дает возможность другому декоратору принимать произвольные аргументы """
+def decorator_with_args_for_any_decorator(decorator_to_enhance: Callable) -> Callable:
+    """ Декоратор. Даёт возможность другому декоратору принимать произвольные аргументы """
     def decorator_maker(*args, **kwargs) -> Callable:
         def decorator_wrapper(func: Callable) -> Callable:
-            return decorator_to_enchance(func, *args, **kwargs)
+            return decorator_to_enhance(func, *args, **kwargs)
         return decorator_wrapper
     return decorator_maker
 
 
 @decorator_with_args_for_any_decorator
 def decorated_decorator(func: Callable, *args, **kwargs) -> Callable:
-    """" Декоратор шаблон """
+    """ Декоратор. Шаблон """
     @functools.wraps(func)
-    def wrapper(*func_args, **func_kwargs) -> Callable:
-        print("Переданные арги в кварги в декоратор: ", dec_args, dec_kwrgs)
-        return func(*func_args, **func_kwargs)
+    def wrapper(function_arg1: Optional[Any], function_arg2: Optional[Any]) -> Callable:
+        print("Переданные арги и кварги:", args, kwargs)
+        return func(function_arg1, function_arg2)
     return wrapper
 
 
 @decorated_decorator(100, 'рублей', 200, 'друзей')
 def decorated_function(text: str, num: int) -> None:
+    """ Пример декорируемой функции """
     print("Привет", text, num)
 
 
